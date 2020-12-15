6cf6bccc78047440158249e8bf6233eeb50091f6
Trương Quang Duy <truongquangduy343@gmail.com>
2020-12-15 13:06:01 +0700

    Login function

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,19 @@
-import axios from 'axios';
-const API_SERVER = 'https://protected-dusk-54874.herokuapp.com/';
-const instance = axios.create({
-	withCredentials: true,
-	baseURL: API_SERVER,
+import axios from "axios";
+const API_SERVER = "http://localhost:5000/";
+let instance = axios.create({
+  withCredentials: true,
+  baseURL: API_SERVER,
 });
 
-export const login = (data) => {
-	return new Promise((resolve, reject) => {
-		instance
-			.post('signin', data)
-			.then((res) => {
-				resolve(res);
-			})
-			.catch((error) => {
-				reject(error);
-			});
-	});
+export const login = async (data) => {
+  return new Promise((resolve, reject) => {
+    instance
+      .post("/signin", data)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 };
