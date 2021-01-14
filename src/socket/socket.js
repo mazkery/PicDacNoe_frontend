e6620c84297807f4def1c1fd2b7223f64db7243b
Mạnh Trọng Lâm Phong <manhtronglamphong@users.noreply.github.com>
2021-01-14 10:29:13 +0700

    - update

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
-const ENDPOINT = " https://protected-dusk-54874.herokuapp.com/ ";
+const ENDPOINT = "https://protected-dusk-54874.herokuapp.com/";
 const socket = socketIOClient(ENDPOINT);
 
 export default socket;
